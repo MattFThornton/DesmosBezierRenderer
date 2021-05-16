4a4b9510bb01ff6fc538f2e1c790535c87e211c4
MattFThornton <Mattf.thornton@gmail.com>
2021-05-16 11:48:26 -0500

    init

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # Desmos Bezier Renderer
 
-A simple image/video to Desmos graph converter run locally. Rotoscopes images using Canny and Potrace edge detection as Bezier curves on Desmos Graphing Calculator.
 
-### Sample
-![](github/sample.png)
+### Input
+![](github/input.png)
 
 ### Result
-![](github/result.png)
+![](github/output.png)
 
 ## Setup
-#### This guide won't work out of the box on Windows. The easiest way to do this on Windows is to [download WSL](https://www.microsoft.com/store/productId/9N6SVWS3RX71) to run all the commands below. You can find it produces under the `\\wsl$\Ubuntu-20.04\home` path on your PC.
 Install dependencies
+
+Debian/ubuntu
+
 ```sh
 apt update
-apt install git python3-dev python3-pip build-essential libagg-dev libpotrace-dev pkg-config
+apt install git python3-dev python3-pip scrot ffmpeg
 ```
+Arch
+
+```sh
+yay -S  git python3 python-pip potrace scrot agg ffmpeg
+```
+
 
 Clone repository
 ```sh
-git clone https://github.com/kevinjycui/DesmosBezierRenderer.git
+git clone https://github.com/MattFThornton/DesmosBezierRenderer
 cd DesmosBezierRenderer
 ```
 
@@ -28,30 +35,32 @@ python3 -m venv env
 . env/bin/activate
 pip3 install -r requirements.txt
 ```
+
 Create a directory called `frames` and add images named `frame%d.png` where `%d` represents the frame-number starting from 1. To render just a single image, add a single image named `frame1.png` in the directory. Works best with 360p to 480p resolution (may have to lower the resolution further with more complex frames). 
+
+You can change the `DYNAMIC_BLOCK`, `BLOCK_SIZE`, and `MAX_EXPR_PER_BLOCK` constants in `backend.py` to change the number of expressions the backend will send to the frontend per call (too much will cause a memory error, too little could kill the backend with too many requests). This only really matters if you are rendering a video.
 ```sh
 mkdir frames
 ...
 ```
-#### Settings
-Constants in the `backend.py` file can be changed to optimise or customise your render. Note that a "block" refers to what is passed from the backend to the frontend per HTTP request.
-| Variable | Type | Description | Notes |
-| --- | --- | --- | --- |
-| `DYNAMIC_BLOCK` | Boolean | Automatically find the right block size | If unsure, leave as true
-| `BLOCK_SIZE` | Integer | Number of frames per block | Ignored if `DYNAMIC_BLOCK` is true |
-| `MAX_EXPR_PER_BLOCK` | Integer | Maximum lines per block, doesn't affect lines per frame | Ignored if `DYNAMIC_BLOCK` is false |
-| `FRAME_DIR` | String | The folder where the frames are stored relative to this file | `'frames'` |
-| `FILE_EXT` | String | Extension for frame files | `'png'`, `'jpg'`, etc. |
-| `COLOUR` | String | Hex value of colour for graph output | Desmos blue is `'#2464b4'` |
-| `DOWNLOAD_IMAGES` | Boolean | Download each rendered frame automatically (works best in Firefox) | If true, each frame is screenshotted automatically. Works best in Firefox, as chromium browsers will constantly prompt for downloads
-| `USE_L2_GRADIENT` | Boolean | Creates less edges but is still accurate (leads to faster renders) | |
-| `SHOW_GRID` | Boolean | Show the grid in the background while rendering | |
+in backend.py, edit the sleepTime variable based on your computer's speed. By default it's set to 90 which works for an old laptop, but for a faster computer, I'd reccomend setting it as 200 or greater(the higher the number the faster it generates the frames)
 
-You can change the `DYNAMIC_BLOCK`, `BLOCK_SIZE`, and `MAX_EXPR_PER_BLOCK` to change the number of expressions the backend will send to the frontend per call (too much will cause a memory error, too little could kill the backend with too many requests). These only really matter if you are rendering a video.
 
 Run backend (This may take a while depending on the size and complexity of the frames). Should eventually show that the server is running on `localhost:5000`.
 ```sh
 python3 backend.py
 ```
 
-Load `index.html` into a web browser and put `f=1` into the first formula in the formula window. The image should start rendering or the video should start playing at a slow rate.
+Load `index.html` into a web browser and run `start(0,0)` in the developer console. The image should start rendering or the video should start playing at a slow rate. If you want to start at a frame that isn't the first, change the first zero in start to the frame you want to start with.
+
+after rendering all the images, use ffmpeg to combine all of them:
+
+```sh
+ffmpeg -r 29.97 -i frame_%003d.png out.mkv
+```
+change the `29.97` to the framerate of the source video to add sound: 
+
+```sh
+ffmpeg -i out.mkv -i sound.mp3 -shortest outWithSound.mkv
+```
+
--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 from flask import Flask
 from flask_cors import CORS
 from flask import request
@@ -8,8 +10,6 @@ import numpy as np
 import potrace
 import cv2
 
-import multiprocessing
-from time import time
 import os
 
 
@@ -17,35 +17,24 @@ app = Flask(__name__)
 CORS(app)
 
 
-DYNAMIC_BLOCK = True # Automatically find the right block size
-BLOCK_SIZE = 25 # Number of frames per block (ignored if DYNAMIC_BLOCK is true)
-MAX_EXPR_PER_BLOCK = 7500 # Maximum lines per block, doesn't affect lines per frame (ignored if DYNAMIC_BLOCK is false)
+DYNAMIC_BLOCK = True
 
-FRAME_DIR = 'frames' # The folder where the frames are stored relative to this file
-FILE_EXT = 'png' # Extension for frame files
-COLOUR = '#2464b4' # Hex value of colour for graph output	
-
-DOWNLOAD_IMAGES = True # Download each rendered frame automatically (works best in firefox)
-USE_L2_GRADIENT = True # Creates less edges but is still accurate (leads to faster renders)
-SHOW_GRID = False # Show the grid in the background while rendering
+BLOCK_SIZE = 25
+MAX_EXPR_PER_BLOCK = 7500
+FRAME_DIR = 'frames'
 
 
-def get_contours(filename, nudge = .33):
+def get_contours(filename):
     image = cv2.imread(filename)
+    cv2.waitKey(0)
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    median = max(10, min(245, np.median(gray)))
-    lower = int(max(0, (1 - nudge) * median))
-    upper = int(min(255, (1 + nudge) * median))
-    filtered = cv2.bilateralFilter(gray, 5, 50, 50)
-    edged = cv2.Canny(filtered, lower, upper, L2gradient = USE_L2_GRADIENT)
-
-    with frame.get_lock():
-        frame.value += 1
-        height.value = max(height.value, image.shape[0])
-        width.value = max(width.value, image.shape[1])
-    print('\r--> Frame %d/%d' % (frame.value, len(os.listdir(FRAME_DIR))), end='')
-
+    edged = cv2.Canny(gray, 30, 200)
+    cv2.waitKey(0)
+    
+    contours, hierarchy = cv2.findContours(edged, 
+        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
     return edged[::-1]
 
 
@@ -53,12 +42,14 @@ def get_trace(data):
     for i in range(len(data)):
         data[i][data[i] > 1] = 1
     bmp = potrace.Bitmap(data)
-    path = bmp.trace(2, potrace.TURNPOLICY_MINORITY, 1.0, 1, .5)
+    path = bmp.trace()
     return path
 
 
 def get_latex(filename):
+
     latex = []
+
     path = get_trace(get_contours(filename))
 
     for curve in path.curves:
@@ -79,34 +70,13 @@ def get_latex(filename):
                 (1-t)((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f))+t((1-t)((1-t)%f+t%f)+t((1-t)%f+t%f)))' % \
                 (x0, x1, x1, x2, x1, x2, x2, x3, y0, y1, y1, y2, y1, y2, y2, y3))
             start = segment.end_point
+
     return latex
 
-def get_expressions(frame):
-    exprid = 0
-    exprs = []
-    for expr in get_latex(FRAME_DIR + '/frame%d.%s' % (frame+1, FILE_EXT)):
-        exprid += 1
-        exprs.append({'id': 'expr-' + str(exprid), 'latex': expr, 'color': COLOUR, 'secret': True})
-    return exprs
+frame_latex = []
 
-frame = multiprocessing.Value('i', 0)
-height = multiprocessing.Value('i', 0, lock = False)
-width = multiprocessing.Value('i', 0, lock = False)
-frame_latex =  range(len(os.listdir(FRAME_DIR)))
-
-with multiprocessing.Pool(processes = multiprocessing.cpu_count()) as pool:
-    print('Desmos Bezier Renderer')
-    print('Junferno 2021')
-    print('https://github.com/kevinjycui/DesmosBezierRenderer')
-
-    print('-----------------------------')
-
-    print('Processing %d frames... Please wait for processing to finish before running on frontend\n' % len(os.listdir(FRAME_DIR)))
-
-    start = time()
-    frame_latex = pool.map(get_expressions, frame_latex)
-    
-    print('\r--> Processing complete in %.1f seconds\n' % (time() - start))
+for i in range(len(os.listdir(FRAME_DIR))):
+    frame_latex.append(get_latex(FRAME_DIR + '/frame%d.png' % (i+1)))
 
 # with open('cache.json', 'w+') as f:
 #     json.dump(frame_latex, f)
@@ -116,7 +86,6 @@ def index():
     frame = int(request.args.get('frame'))
     if frame >= len(os.listdir(FRAME_DIR)):
         return {'result': None}
-
     block = []
     if not DYNAMIC_BLOCK:
         number_of_frames = min(frame + BLOCK_SIZE, len(os.listdir(FRAME_DIR))) - frame
@@ -133,10 +102,20 @@ def index():
             total += len(frame_latex[i])
             block.append(frame_latex[i])
             i += 1
-    return json.dumps({'result': block, 'number_of_frames': number_of_frames}) # Number_of_frames is the number of newly loaded frames, not the total frames
+    return json.dumps({'result': block, 'number_of_frames': number_of_frames})
 
-@app.route('/init')
-def init():
-    return json.dumps({'height': height.value, 'width': width.value, 'total_frames': len(os.listdir(FRAME_DIR)), 'download_images': DOWNLOAD_IMAGES, 'show_grid': SHOW_GRID})
 
+@app.route('/process')
+def process():
+    waitTime = 90
+    sleep = int(request.args.get('sleep'))
+    frame = str(request.args.get('frame'))
+    if sleep/waitTime > 5:
+        time.sleep(sleep/waitTime)
+    else:
+        time.sleep(5)
+    os.system("scrot output/frame_"+frame+".png")
+    # ffmpeg -r 29.97 -i frame_%003d.png test.mkv 
+    # -r = the framerate, adjust when needed
+    return "frame_" + frame + ".png saved"
 app.run()
